e99f6c7914a8d656ba8ed40e6369c6aa1efbf121
Morteza H. Siboni <hakimm2@rpi.edu>
2022-03-10 19:05:46 -0500

    Adds scorec setup files

---
+++ env-setup-scorec.sh
@@ -0,0 +1,7 @@
+#setup module load cmake
+module load \
+gcc/7.3.0-bt47fwr \
+mpich/3.3-diz4f6i \
+metis/5.1.0-int32-b3l465o \
+cmake/3.12.1-wfk2b7e \
+hypre/2.15.1-int32-uhae7i4
---
+++ scorec_config.sh
@@ -0,0 +1,14 @@
+export MFEM_INSTALL_PATH=/lore/hakimm2/opt/mfem_no_pumi
+export HYPRE_DIR=$HYPRE_ROOT
+export METIS_DIR=$METIS_ROOT
+flags="-g -O0"
+cmake .. \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DMFEM_PREFIX=$MFEM_INSTALL_PATH \
+  -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF \
+  -DCMAKE_INSTALL_PREFIX:PATH="$CMAKE_BINARY_DIR/../bin" \
+  -DCMAKE_C_COMPILER="mpicc" \
+  -DCMAKE_CXX_COMPILER="mpicxx" \
+  -DCMAKE_C_FLAGS="${flags}" \
+  -DCMAKE_CXX_FLAGS="${flags}" \
+  -DCMAKE_EXE_LINKER_FLAGS="-lpthread ${flags}"
